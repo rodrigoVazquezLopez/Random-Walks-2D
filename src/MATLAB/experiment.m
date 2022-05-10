a0f976da464d4dce08ab6ef428cafbfd514a4ccd
Rodrigo Vázquez <rodrigo_em2@hotmail.com>
2022-05-10 18:35:24 -0500

    commit

--- src/MATLAB/experiment.m
+++ src/MATLAB/experiment.m
@@ -1,4 +1,4 @@
-obsFile = fopen("..\..\data\Experiment\obstacles_03May2022_12.40.txt","r");
+obsFile = fopen("..\..\data\Experiment\obstacles_09May2022_12.17.txt","r");
 obs = fscanf(obsFile, "(%f, %f), %f,\n", [3,inf]);
 obs = obs';
 obs(:,1) = obs(:,1)/100;
@@ -15,21 +15,21 @@ yticks(-2.05:0.1:2.05);
 s.AlphaData = obs(:,3);
 s.MarkerFaceAlpha = 'flat';
 
-rndwlk = readmatrix("..\..\data\Experiment\randomWalk_03May2022_12.46.txt");
+rndwlk = readmatrix("..\..\data\Experiment\randomWalk_09May2022_12.17.txt");
 rndwlk = rndwlk/100;
 
 plot(rndwlk(:,1),rndwlk(:,2), '-s','LineWidth',2, 'MarkerSize',10)
 
-solution = readmatrix("..\..\data\Experiment\result_03May2022_12.46.txt");
+solution = readmatrix("..\..\data\Experiment\result_09May2022_12.17.txt");
 solution = solution/100;
 
 plot(solution(:,1),solution(:,2), '-s','LineWidth',2, 'MarkerSize',10)
 
-dron1 = readmatrix("..\..\data\Experiment\Dron1_Vuelo_03May2022_12.46_log.txt");
+dron1 = readmatrix("..\..\data\Experiment\Dron1_Vuelo_09May2022_12.17_log.txt");
 
 plot(dron1(:,2),dron1(:,3), '-','LineWidth',2);
 
-dron2 = readmatrix("..\..\data\Experiment\Dron2_Vuelo_03May2022_12.46_log.txt");
+dron2 = readmatrix("..\..\data\Experiment\Dron2_Vuelo_09May2022_12.17_log.txt");
 
 plot(dron2(:,2),dron2(:,3), '-','LineWidth',2);
 
--- src/MATLAB/points2D.m
+++ src/MATLAB/points2D.m
@@ -43,7 +43,8 @@ alpha = zeros(idx-1,1);
 maxVal = max(F(:,3));
 
 for i = 1:idx-1
-    R(i,3) = ((R(i,3) * 1) / maxVal);
+%     R(i,3) = ((R(i,3) * 1) / maxVal);
+    R(i,3) = R(i,3) / j
 end
 
 writematrix(R,'puntos.txt','Delimiter',',');
@@ -73,5 +74,65 @@ hold on
 solution = readmatrix("..\..\solution.txt");
 plot(solution(:,1)/100,solution(:,2)/100, '-s','LineWidth',2, 'MarkerSize',10)
 
+figure
+grid on;
+
+[X,Y] = meshgrid(R(:,1),R(:,2));
+Z = (X + Y)*0 + 1;
+[Zf,Zc] = size(Z);
+Z(112, 112) = 0.5; %
+Z(112, 113) = 0.5; %
+Z(112, 111) = 0.5; %
+Z(113, 112) = 0.5; %
+Z(113, 113) = 0.5; %
+Z(113, 111) = 0.5; %
+Z(111, 112) = 0.5; %
+Z(111, 113) = 0.5; %
+Z(111, 111) = 0.5; %
+mesh(X,Y,Z)
+%plot3(R(:,1),R(:,2),R(:,3),"-s",LineStyle="none",MarkerFaceColor=[0,0,0.7])
+%bar3(R)
+
+Z = zeros(31,25);
+X = zeros(31,25);
+Y = zeros(31,25);
+[X,Y] = meshgrid( -1.0:0.1:1.4, -1.2:0.1:1.8);
+
+%resultados = 0;
+c= 0;
+c2=0;
+for i = -1.2:0.1:1.8
+    c = c+1;
+    c2 = 0;
+    for j= -1.0:0.1:1.4
+        c2 = c2 + 1;
+        for k=1:223
+
+            %X(c,) = R(k,1);
+            %Y(c,c2) = R(k,2)
+            %if(i==R(k,1) && j==R(k,2))
+            if(i==X(c,c2) )
+                Z(c,c2) = R(k,3);
+                %Z(:,c2) = R(k,3)
+               
+                %resultados(k,1:3) = [i j Z(c, c2)];
+            end
+        end
+    end
+    
+end
+
+
+
+surf(X,Y,Z)
+
+% Z = sin(X) + cos(Y);
+% C = X.*Y;
+% surf(X,Y,Z,C)
+% colorbar
+
+hold on
+z = zeros(37,1);
+plot3(solution(:,1)/100,solution(:,2)/100,z,'-s','LineWidth',2, 'MarkerSize',10)
 
 
